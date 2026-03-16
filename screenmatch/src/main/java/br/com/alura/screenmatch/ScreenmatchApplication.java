f24e38e56fe3fbd8c48327ce3b53b2501998baa5
GersonAparecidoLima <gersonaparecido@gmail.com>
2026-03-16 14:42:39 -0300

    cria mapeamento de DadosSerie e integração com Jackson

--- screenmatch/src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ screenmatch/src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,6 +1,8 @@
 package br.com.alura.screenmatch;
 
+import br.com.alura.screenmatch.model.DadosSerie;
 import br.com.alura.screenmatch.service.ConsumoAPI;
+import br.com.alura.screenmatch.service.ConverteDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -18,7 +20,10 @@ public class ScreenmatchApplication implements CommandLineRunner {
 		//	String endereco = "https://www.omdbapi.com/?t=" + busca.replace(" ", "+") + "&apikey=c727e294";
 		var json = consumoapi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=c727e294");
 		System.out.println(json);
-		json = consumoapi.obterDados("https://coffee.alexflipnote.dev/random.json");
-		System.out.println(json);
+		//json = consumoapi.obterDados("https://coffee.alexflipnote.dev/random.json");
+		//System.out.println(json);
+
+		  ConverteDados conversor = new ConverteDados();
+		  DadosSerie dados = conversor.obterDados(json,DadosSerie.class);
 	}
 }
---
+++ screenmatch/src/main/java/br/com/alura/screenmatch/model/DadosSerie.java
@@ -0,0 +1,18 @@
+package br.com.alura.screenmatch.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.util.IgnorePropertiesUtil;
+
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DadosSerie(@JsonAlias("Title") String titulo,
+                         @JsonAlias("totalSeasons") Integer totalTemporadas,
+                         @JsonAlias("imdbRating") String avaliacao
+                         ) {
+}
+
+
